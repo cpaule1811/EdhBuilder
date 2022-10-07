88d5dabd7376283307626fd994fdc77373d46b02
Charlie Edwards <charles.edwards1811@gmail.com>
2022-10-07 20:23:37 +1000

    Deleted v1 EdhBuilder project.

--- src/components/Message/Message.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Message.css";
-
-function Message({ message, color }) {
-  return <div className={`message ${color}`}>{message}</div>;
-}
-
-export default Message;
--- src/components/spinner/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import spinner from "../../icons/spinner.svg";
-
-function Spinner() {
-  return <img className="spinner" src={spinner} alt="loading spinner" />;
-}
-
-export default Spinner;
