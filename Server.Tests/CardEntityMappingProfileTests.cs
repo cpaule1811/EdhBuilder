aa133cf3b1ebf03aaa0c18c88b43f799ce65ad30
Charlie Edwards <charles.edwards1811@gmail.com>
2022-10-09 11:50:25 +1000

    (#3) Add automapper to convert card json dtos to a model.

---
+++ Server.Tests/CardEntityMappingProfileTests.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using AutoMapper;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Server.Controllers;
+using Server.Dtos;
+using Card = Server.Dtos.Card;
+
+namespace Server.Tests;
+
+public class CardEntityMappingProfileTests
+{
+    private const string TestCardDtosPath = "TestData/cards.json";
+    private IEnumerable<CardDto> _testCardDtos;
+    private IMapper _mapper;
+
+    [OneTimeSetUp]
+    public void Init() {
+        var configuration = new MapperConfiguration(cfg => {
+            cfg.AddProfile<CardEntityMappingProfile>();
+        });
+        
+        _mapper = new Mapper(configuration);
+    }
+
+    [SetUp]
+    public void Setup() {
+        var CardJsonReader = new CardJsonReader(new FileInfo(TestCardDtosPath));
+
+        _testCardDtos = CardJsonReader.Read();
+    }
+
+    [Test]
+    public void CardMap_WhenGivenDto_ShouldReturnExpectedResult() {
+        var expected = new Card {
+            OracleId = Guid.ParseExact("09cc8709-fe10-472a-b05c-e89f3523018d", "D"),
+            Name = "Austere Command",
+            Lang = "en",
+            Layout = "normal",
+            Power = null,
+            Toughness = null,
+            Colors = new []{ "W" },
+            ColorIndicator = Array.Empty<string>(),
+            ColorIdentity = new []{ "W" },
+            Keywords = Array.Empty<string>(),
+            AllParts = Array.Empty<AllPart>(),
+            Legalities = new Legalities {
+                Standard = false,
+                Pioneer = false,
+                Modern = true,
+                Legacy = true,
+                Vintage = true,
+                Commander = true,
+                Brawl = false
+            },
+            CardVersions = new CardVersion[] {
+                new () {
+                    Artist = "Anna Steinbauer",
+                    IsPromo = false,
+                    IsVariation = false,
+                    IsFullArt = false,
+                    IsFoil = false,
+                    IsReprint = true,
+                    BorderColor = "black",
+                    Set = "clb",
+                    Rarity = "rare",
+                    SetName = "Commander Legends: Battle for Baldur's Gate",
+                    CardImage = new Uri("https://cards.scryfall.io/normal/front/3/d/3d24036e-075f-4991-a740-a9d943722ad2.jpg?1654114687"),
+                    ArtCrop = new Uri("https://cards.scryfall.io/art_crop/front/3/d/3d24036e-075f-4991-a740-a9d943722ad2.jpg?1654114687"),
+                },
+                new () {
+                    Artist = "Wayne England",
+                    IsPromo = false,
+                    IsVariation = false,
+                    IsFullArt = false,
+                    IsFoil = false,
+                    IsReprint = true,
+                    BorderColor = "black",
+                    Set = "cmd",
+                    Rarity = "rare",
+                    SetName = "Commander 2011",
+                    CardImage = new Uri("https://cards.scryfall.io/normal/front/4/5/45000021-a6d9-4f86-a92e-3e52d1000c20.jpg?1592712728"),
+                    ArtCrop = new Uri("https://cards.scryfall.io/art_crop/front/4/5/45000021-a6d9-4f86-a92e-3e52d1000c20.jpg?1592712728"),
+                }
+            }
+        };
+        var expectedAsJson = JsonConvert.SerializeObject(expected);
+        
+        var result = _mapper.Map<Card>(_testCardDtos);
+        var resultAsJson = JsonConvert.SerializeObject(result);
+        
+        Assert.AreEqual(expectedAsJson, resultAsJson);
+    }
+}
---
+++ Server/Models/Card.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+
+namespace Server.Dtos;
+
+public class Card
+{
+    public Guid OracleId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Lang { get; set; } = string.Empty;
+    public string Layout { get; set; } = string.Empty;
+    public string Power { get; set; } = string.Empty;
+    public string TypeLine { get; set; } = string.Empty;
+    public string Toughness { get; set; } = string.Empty;
+    public IEnumerable<string> Colors { get; set; } = Enumerable.Empty<string>();
+    public IEnumerable<string> ColorIndicator { get; set; } = Enumerable.Empty<string>();
+    public IEnumerable<string> ColorIdentity { get; set; } = Enumerable.Empty<string>();
+    public IEnumerable<string> Keywords { get; set; } = Enumerable.Empty<string>();
+    public IEnumerable<AllPart> AllParts { get; set; } = Enumerable.Empty<AllPart>();
+    public Legalities Legalities { get; set; } = new ();
+    public IEnumerable<CardVersion> CardVersions { get; set; } = Enumerable.Empty<CardVersion>();
+}
+
+public class AllPart
+{
+    public Guid Id { get; set; }
+}
+
+public class ImageUris
+{
+    [JsonProperty("normal")] 
+    public Uri Normal { get; set; } 
+
+    [JsonProperty("art_crop")]
+    public Uri ArtCrop { get; set; }
+}
+
+public class Legalities
+{
+    public bool Standard { get; set; }
+    public bool Pioneer { get; set; }
+    public bool Modern { get; set; }
+    public bool Legacy { get; set; }
+    public bool Vintage { get; set; }
+    public bool Commander { get; set; }
+    public bool Brawl { get; set; }
+}
+
+public class CardVersion
+{
+    public string Set { get; set; }
+    public string SetName { get; set; }
+    public Uri CardImage { get; set; }
+    public Uri ArtCrop { get; set; }
+    public string Artist { get; set; }
+    public bool IsFoil { get; set; }
+    public bool IsPromo { get; set; }
+    public bool IsVariation { get; set; }
+    public bool IsFullArt { get; set; }
+    public bool IsReprint { get; set; }
+    // TODO: Maybe enum??
+    public string Rarity { get; set; }
+    public string BorderColor { get; set; }
+}
